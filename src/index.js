ba5b5ac05dd299dc4618e849bb37edc10057cb72
Alex Robson <asrobson@gmail.com>
2014-05-28 13:42:12 -0500

    Improve API to derive appName (for bucket prefixing) from autohost or config and return an object containing authorization, authentication and hasUsers properties

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
 var when = require( 'when' );
 
-module.exports = function( autohost, config, done ) {
+module.exports = function( host, config, done ) {
 	return when.promise( function( resolve, reject ) {
-		var riak 			= require( './riak.js' )( config, function() {
+		var riak 			= require( './riak.js' )( host, config, function( hasUsers ) {
 			authentication 	= require( './authentication.js' )( riak ),
 			authorization 	= require( './authorization.js' )( riak ),
 			passport = require( 'passport' ),
 			BasicStrategy = require( 'passport-http' ).BasicStrategy;
-			autohost.withAuthenticationProvider( authentication );
-			autohost.withPassportStrategy(
+			host.withAuthenticationProvider( authentication );
+			host.withPassportStrategy(
 				new BasicStrategy( {}, authentication.verify ),
 				passport.authenticate( 'basic', { session: config.disable_sessions } ),
 				/^[\/]anon.*/ );
-			autohost.withAuthorizationProvider( authorization );
-			resolve();
+			host.withAuthorizationProvider( authorization );
+			var result = {
+				authentication: authentication,
+				authorization: authorization,
+				hasUsers: hasUsers
+			};
+			resolve( result );
 			if( done ) {
-				done();
+				done( result );
 			}
 		} );
 	} );
